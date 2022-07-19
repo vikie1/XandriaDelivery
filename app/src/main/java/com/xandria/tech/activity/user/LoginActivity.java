fa52bbdc426d19a1496fa425857c5e0f825fb822
vikie1 <39300268+vikie1@users.noreply.github.com>
2022-07-19 15:04:29 +0300

    initial commit

---
+++ app/src/main/java/com/xandria/tech/activity/user/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.xandria.tech.activity.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xandria.tech.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
